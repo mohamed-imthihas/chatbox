6901a6ff9585147222d598d89c1f622f6f1c6550
Mohamed Imthihas <imthihas94@gmail.com>
2017-08-06 22:17:20 +0530

    Add files via upload

--- client/app/login/authentication.service.ts
+++ client/app/login/authentication.service.ts
@@ -3,12 +3,14 @@ import { Http, Headers, Response } from "@angular/http";
 import { CanActivate }    from '@angular/router';
 import 'rxjs/add/operator/toPromise';
 import { Observable } from "rxjs";
-
+import { Subject }    from 'rxjs/Subject';
 import { User } from "./user.model";
 
 @Injectable()
 export class AuthenticationService implements CanActivate{
 	currentUser:User;
+	 private userDisconnect = new Subject<any>();
+	 userDisconnect$ = this.userDisconnect.asObservable();
 	constructor(private http:Http){
 		this.currentUser = JSON.parse(sessionStorage.getItem("user"));
 	}
@@ -20,6 +22,7 @@ export class AuthenticationService implements CanActivate{
              	if(result.logged){
              		this.currentUser = {fullName:result.fullName,email:result.email};
              		sessionStorage.setItem("user",JSON.stringify(this.currentUser));
+             		this.userDisconnect.next();
              	}
              	return result.logged;
              });
@@ -28,7 +31,7 @@ export class AuthenticationService implements CanActivate{
 	signUp(user:User){
 		return this.http.post("/newUser",user).toPromise().then(response => {
 			let result = response.json();
-            return result.status == 'success';
+            return result.status != 'failed';
 		});
 	}
 	getLoggedUser():User{
@@ -42,5 +45,10 @@ export class AuthenticationService implements CanActivate{
 	}
 	checkEmail(email:string):Observable{
 		return this.http.post("/checkEmail",{email:email});
+	}
+	loggedOut(){
+		sessionStorage.removeItem("user");
+		this.currentUser = null;
+		this.userDisconnect.next();
 	}
 }
